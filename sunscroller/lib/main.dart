c9de2889290abc73d9d4051aec5afdbdd15161d0
Moon_Star32 <azamat.yusupov.04@outlook.com>
2024-09-22 01:51:35 +0300

    fix

--- sunscroller/lib/main.dart
+++ sunscroller/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(home: SunPage()));
-}
+void main() => runApp(
+      const MaterialApp(
+        home: SunPage(),
+      ),
+    );
 
 class SunPage extends StatefulWidget {
   const SunPage({super.key});
@@ -27,27 +29,35 @@ class SunPageState extends State<SunPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: AnimatedContainer(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            duration: const Duration(milliseconds: 500),
-            color: getColor(),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                      width: 50,
-                      height: 50,
-                      margin: EdgeInsets.only(top: 50 + sunState * 4),
-                      decoration: BoxDecoration(
-                          color: Colors.yellow,
-                          borderRadius: BorderRadius.circular(25))),
-                  Slider(
-                      value: sunState,
-                      max: 100,
-                      activeColor: Colors.black,
-                      inactiveColor: Colors.white,
-                      onChanged: (value) => setState(() => sunState = value))
-                ])));
+      body: AnimatedContainer(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        duration: const Duration(milliseconds: 500),
+        color: getColor(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              margin: EdgeInsets.only(top: 50 + sunState * 4),
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(25),
+              ),
+            ),
+            Slider(
+              value: sunState,
+              max: 100,
+              activeColor: Colors.black,
+              inactiveColor: Colors.white,
+              onChanged: (value) => setState(
+                () => sunState = value,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
